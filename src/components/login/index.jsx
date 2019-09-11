3bd1eace343f2a5ff82a8d312bd3735979f02de4
pang <1793115617@qq.com>
2019-09-11 23:27:54 +0800

    login 摁下登录跳转到 home组件

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
-import {Form, Icon, Input, Button} from 'antd';
+import {Form, Icon, Input, Button, message} from 'antd';
+import axios from 'axios';
 
 import logo from './logo.png';
 import './index.less';
@@ -8,21 +9,48 @@ import './index.less';
 @Form.create()
 class Login extends Component {
     //自定义校验将表单的方法
-    validator=(rule, value, callback)=>{
-        const name=rule.field==='username'? '用户名':'密码';
-        if(!value){
-          return callback(`请输入${name}`)
+    validator = (rule, value, callback) => {
+        const name = rule.field === 'username' ? '用户名' : '密码';
+        if (!value) {
+            return callback(`请输入${name}`)
         }
-        if(value.length>13){
+        if (value.length > 13) {
             return callback(`${name}长度小于13位`)
         }
-        if(value.length<3){
+        if (value.length < 3) {
             return callback(`${name}长度大于3位`)
         }
-        const reg=/^[a-zA-Z0-9_]{3,13}$/;
-      if(!reg.test(value)){
-          return callback(`${name}只能为英文字母，数字，下划线`)
-      }
+        const reg = /^[a-zA-Z0-9_]{3,13}$/;
+        if (!reg.test(value)) {
+            return callback(`${name}只能为英文字母，数字，下划线`)
+        }
+        callback();
+    };
+//登录验证函数
+    login = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                //如果校验成功，拿到表单数据，发送请求
+                const {username, password} = values;
+                axios.post('http://localhost:3000/api/login', {username, password})
+                    .then((response) => {
+                        if (response.data.status === 0) {
+                            //登录成功
+                            message.success('登录成功了~');
+                            this.props.history.replace('/');
+
+                        } else {
+                            //登录失败了
+                            message.error(response.data.msg);
+                        }
+                    })
+                    .catch((error) => {
+                        //请求失败-登录失败
+                        message.error('未知错误，请联系管理人员');
+                    })
+            }
+        })
     };
 
     render() {
